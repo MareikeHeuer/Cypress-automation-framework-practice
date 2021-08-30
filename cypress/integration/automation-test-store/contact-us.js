f044a33e8653e7370b3d93a7b388e542059a3f65
MareikeHeuer <mareike.heuer@hotmail.com>
2021-08-30 22:57:31 +0200

    Download Cypress XPath package

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,7 +3,8 @@
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("https://www.automationteststore.com/");
-    cy.get(".info_links_footer > :nth-child(5) > a").click();
+    // cy.get(".info_links_footer > :nth-child(5) > a").click();
+    cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("John");
     cy.get("#ContactUsFrm_email").type("john@hotmail.com");
     cy.get("#ContactUsFrm_enquiry").type("This is my comment");
