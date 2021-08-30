177b8e92a08a5d6f539afb08790066cf54a5a48c
MareikeHeuer <mareike.heuer@hotmail.com>
2021-08-30 14:14:01 +0200

    Start new contact-us form test for Automation Test Store

---
+++ cypress/integration/automation-test-store/contact-us.js
@@ -0,0 +1,7 @@
+/// <reference types="Cypress" />
+
+describe("Test Contact Us form via Automation Test Store", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+    cy.visit("https://www.automationteststore.com/");
+  });
+});
