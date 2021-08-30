76adeccf5f0d9b8ab2e96ff8d5a83e4ec3a6c9fa
MareikeHeuer <mareike.heuer@hotmail.com>
2021-08-30 14:51:02 +0200

    Add more test cases to Automation Test Store contact us form submission

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,5 +3,10 @@
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("https://www.automationteststore.com/");
+    cy.get(".info_links_footer > :nth-child(5) > a").click();
+    cy.get("#ContactUsFrm_first_name").type("John");
+    cy.get("#ContactUsFrm_email").type("john@hotmail.com");
+    cy.get("#ContactUsFrm_enquiry").type("This is my comment");
+    cy.get(".col-md-6 > .btn").click();
   });
 });
