c64b8632932c4ebf6b9e7e21249a89acb96ecd37
MareikeHeuer <mareike.heuer@hotmail.com>
2021-08-30 23:16:36 +0200

    Creating more dynamic selectors

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -3,11 +3,11 @@
 describe("Test Contact Us form via Automation Test Store", () => {
   it("Should be able to submit a successful submission via contact us form", () => {
     cy.visit("https://www.automationteststore.com/");
-    // cy.get(".info_links_footer > :nth-child(5) > a").click();
-    cy.xpath("//a[contains(@href, 'contact')]").click();
+    cy.get("a[href$='contact']").click();
+    // cy.xpath("//a[contains(@href, 'contact')]").click();
     cy.get("#ContactUsFrm_first_name").type("John");
     cy.get("#ContactUsFrm_email").type("john@hotmail.com");
     cy.get("#ContactUsFrm_enquiry").type("This is my comment");
-    cy.get(".col-md-6 > .btn").click();
+    cy.get("button[title='Submit']").click();
   });
 });
