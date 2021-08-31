b8b78ea9ec77ae3bba572029df25701567fc9156
MareikeHeuer <mareike.heuer@hotmail.com>
2021-08-31 12:08:03 +0200

    Add assertion to existing test

--- cypress/integration/automation-test-store/contact-us.js
+++ cypress/integration/automation-test-store/contact-us.js
@@ -9,5 +9,9 @@ describe("Test Contact Us form via Automation Test Store", () => {
     cy.get("#ContactUsFrm_email").type("john@hotmail.com");
     cy.get("#ContactUsFrm_enquiry").type("This is my comment");
     cy.get("button[title='Submit']").click();
+    cy.get(".mb40 > :nth-child(3)").should(
+      "have.text",
+      "Your enquiry has been successfully sent to the store owner!"
+    ); // successful test notification
   });
 });
